ae8b0ba1a9e59297df6e261c2e64aec787f7bf49
Michael Handumon <kmcaelian@gmail.com>
2022-10-14 15:18:50 +0800

    feat: switch character controller

--- Assets/Scripts/SwitchCharacterScript.cs
+++ Assets/Scripts/SwitchCharacterScript.cs
@@ -3,43 +3,50 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.InputSystem;
 
 public class SwitchCharacterScript : MonoBehaviour
 {
     public GameObject[] characters;
     GameObject currentCharacter;
-    // public ThirdPersonController thirdPersonController;
-    Transform characterRoot;
+
+    [SerializeField] public GameObject followCamera; 
+    // private ThirdPersonController thirdPersonController;
+    // Transform characterRoot;
     int characterIndex;
 
-    // void Awake() {
-    //     thirdPersonController = currentCharacter.GetComponent<ThirdPersonController>();
-    // }
+    void Awake() {
+        // thirdPersonController = currentCharacter.GetComponent<ThirdPersonController>();
+    }
 
     void Start() {
         characterIndex = 0;
-        // currentCharacter = characters[0];
+        currentCharacter = characters[0];
     }
 
     void Update () {
         if(Input.GetButtonDown("Fire1")){
             characterIndex++;
+            
             if(characterIndex == characters.Length){
                 characterIndex = 0;
             };
-            Console.WriteLine(currentCharacter);
-            currentCharacter.GetComponent<Animator>().enabled = false;
+
+            followCamera.GetComponent<CinemachineVirtualCamera>().LookAt = characters[characterIndex].transform.GetChild(0);
+            followCamera.GetComponent<CinemachineVirtualCamera>().Follow = characters[characterIndex].transform.GetChild(0);
+
+            // Console.WriteLine(currentCharacter);
+            // currentCharacter.GetComponent<Animator>().enabled = false;
             currentCharacter.GetComponent<CharacterController>().enabled = false;
             // currentCharacter.GetComponent<ThirdPersonController>().enabled = false;
-            // currentCharacter.GetComponent<PlayerInput>().enabled = false;
+            currentCharacter.GetComponent<PlayerInput>().enabled = false;
 
-            characters[characterIndex].GetComponent<Animator>().enabled = true;
+            // characters[characterIndex].GetComponent<Animator>().enabled = true;
             characters[characterIndex].GetComponent<CharacterController>().enabled = true;
             // characters[characterIndex].GetComponent<ThirdPersonController>().enabled = true;
-            // characters[characterIndex].GetComponent<PlayerInput>().enabled = true;
+            characters[characterIndex].GetComponent<PlayerInput>().enabled = true;
 
             // characterRoot = characters[characterIndex].transform.GetChild(0);
-            GameObject.Find("PlayerFollowCamera").GetComponent<CinemachineVirtualCamera>().Follow = characters[characterIndex].transform;
             currentCharacter = characters[characterIndex];
         }
     }
